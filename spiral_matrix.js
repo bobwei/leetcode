22d46b79195522d96c27fd240b51ed6dbe8c352b
bobwei <bob61016@gmail.com>
2015-03-24 17:21:20 +0800

    solve spiral_matrix

--- spiral_matrix.js
+++ spiral_matrix.js
@@ -13,39 +13,36 @@ m rows, n columns
 ]
 
 should return [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+
 */
 var spiralOrder = function(matrix) {
-    var m_max = matrix.length;
-    var m_min = 0;
-    var n_max = matrix[0].length;
-    var n_min = 0;
+    var mMax = matrix.length;
+    var mMin = 0;
+    var nMax = matrix[0].length - 1;
+    var nMin = -1;
     var x = 0;
     var y = 0;
-    var deltaX;
-    var deltaY;
+    var deltaX = 1;
+    var deltaY = 0;
     var output = [];
     for (var i = 0 ; i <  matrix.length * matrix[0].length; i ++){
-        // console.log('i, matrix[y][x] = ' + i + ', ' + matrix[y][x]);
         output.push(matrix[y][x]);
-        if (x === n_min && y === m_min){
+        if (x === nMin && y === mMin){
             deltaX = 1;
             deltaY = 0;
-            n_max -= 1;
-        }else if (x === n_max && y === m_min){
+            nMax -= 1;
+        }else if (x === nMax && y === mMin){
             deltaX = 0;
             deltaY = 1;
-            m_max -= 1;
-        }else if (x === n_max && y === m_max){
+            mMax -= 1;
+        }else if (x === nMax && y === mMax){
             deltaX = -1;
             deltaY = 0;
-            // n_min += 1;
-        }else if (x === n_min && y === m_max){
+            nMin += 1;
+        }else if (x === nMin && y === mMax){
             deltaX = 0;
             deltaY = -1;
-            m_min += 1;
-            // m_max -= 1;
-            // n_min += 1;
-            // n_max -= 1;
+            mMin += 1;
         }
         x += deltaX;
         y += deltaY;
@@ -67,8 +64,6 @@ var genMatric = function(k){
     return rows;
 };
 
-var matrix = genMatric(5);
+var matrix = genMatric(10);
 console.log(matrix);
-
 console.log(spiralOrder(matrix));
-// console.log(genMatric(5));
