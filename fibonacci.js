c6b786d07a9182b8c031e7e5b4b9f6bbbb329a32
bobwei <bob61016@gmail.com>
2015-04-02 12:02:35 +0800

    add fibonacii

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+var fibonacci = function(n){
+    var results = {
+        '1': 1,
+        '2': 2
+    };
+
+    for ( var i = 3 ; i <= n ; i++ ){
+        results[i] = results[i-2] + results[i-1];
+    }
+
+    return results[n];
+};
+
+console.log(fibonacci(30));
