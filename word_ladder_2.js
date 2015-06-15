3a773938052f7954a4d98e057158a0a445e34e09
bobwei <bob61016@gmail.com>
2015-06-15 01:39:15 -0700

    add word_ladder and word_ladder2

--- word_ladder_2.js
+++ word_ladder_2.js
@@ -1,108 +1,89 @@
+/*jshint loopfunc: true */
+
 'use strict';
 
-/**
-* @param {string} start
-* @param {string} end
-* @param {set} dict
-* @return {string[][]}
-*/
-
-/*
-
-    start = 'hit'
-    end = 'cog'
-    dict = ['hot','dot','dog','lot','log']
-    Return
-      [
-        ['hit','hot','dot','dog','cog'],
-        ['hit','hot','lot','log','cog']
-      ]
-
-*/
-
 var isMatched = function(word1, word2){
-    var diffCount = 0;
-    for ( var i = 0 ; i < word1.length ; i++ ){
-        if (word1[i] !== word2[i]){
-            diffCount++;
-            if (diffCount > 1){
-
-                return false;
-            }
-        }
+  var diffCount = 0;
+  for ( var i = 0 ; i < word1.length ; i++ ){
+    if (word1[i] !== word2[i]){
+      diffCount++;
+      if (diffCount > 1){
+        return false;
+      }
     }
-
-    return true;
+  }
+  return true;
 };
 
-var buildGraph = function(end, start, dict){
-    var dictRecordLevel = dict.reduce(function(s, v){
-        s[v] = -1;
+var buildGraph = function(end, dictSet){
+  var dict = [];
+  dictSet.forEach(function(key){
+    dict.push(key);
+  });
+  var distances = {};
+  var graph = {};
+  dict.forEach(function(key){
+    distances[key] = Infinity;
+    graph[key] = [];
+  });
+  distances[end] = 0;
 
-        return s;
-    }, {});
-    var dictRecord = {};
-    var queue = [end];
-    dictRecordLevel[end] = 0;
-    while (queue.length){
-        var currentWord = queue.shift();
-        dict.filter(function(wordInDict){
-
-            return dictRecordLevel[wordInDict] < 0 || dictRecordLevel[wordInDict] > dictRecordLevel[currentWord];
-        }).forEach(function(wordInDict){
-            if (isMatched(currentWord, wordInDict)){
-                queue.push(wordInDict);
-                dictRecordLevel[wordInDict] = dictRecordLevel[currentWord] + 1;
-                if (!dictRecord[wordInDict]){
-                    dictRecord[wordInDict] = [];
-                }
-                if (dictRecord[wordInDict].indexOf(currentWord) < 0){
-                    dictRecord[wordInDict].push(currentWord);
-                }
-            }
-        });
-        if (isMatched(currentWord, start)){
-            if (!dictRecord[start]){
-                dictRecord[start] = [];
-            }
-            if (dictRecord[start].indexOf(currentWord) < 0){
-                dictRecord[start].push(currentWord);
-            }
+  var queue = [end];
+  while (queue.length){
+    var current = queue.shift();
+    // for all adjacent nodes
+    dict
+      .filter(function(key){
+        return isMatched(key, current) && key !== current;
+      })
+      .forEach(function(key){
+        var distance = distances[current] + 1;
+        if (distance <= distances[key]){
+          distances[key] = distance;
+          graph[key].push(current);
+          queue.push(key);
         }
-    }
+      });
+  }
 
-    return dictRecord;
+  return graph;
 };
 
-var getPaths = function(start, graph){
-    var paths = [];
-    var stack = [[start]];
-    var shortestPathLength = -1;
-    while (stack.length){
-        var path = stack.pop();
-        if (graph[path.slice(-1)[0]] && (( shortestPathLength < 0 ) || ( shortestPathLength > 0 && path.length < shortestPathLength ))){
-            graph[path.slice(-1)[0]].forEach(function(child){
-                var tmpPath = path.slice();
-                tmpPath.push(child);
-                stack.push(tmpPath);
-            });
-        }else{
-            paths.push(path);
-            shortestPathLength = path.length;
-        }
+var getPaths = function(start, end, graph, dictSet){
+  var output = [];
+  var dict = [];
+  dictSet.forEach(function(key){
+    dict.push(key);
+  });
+  graph[start] = dict.filter(function(key){
+    return isMatched(key, start) && key !== start;
+  });
+
+  var minLength = Infinity;
+  var stack = [[start]];
+  while (stack.length){
+    var currentPath = stack.pop();
+    var lastWord = currentPath.slice(-1)[0];
+    if (lastWord !== end && currentPath.length <= minLength){
+      graph[lastWord].forEach(function(key){
+        var tmpPath = currentPath.slice();
+        tmpPath.push(key);
+        stack.push(tmpPath);
+      });
+    }else if (lastWord === end && currentPath.length <= minLength){
+      minLength = currentPath.length;
+      output.push(currentPath);
     }
+  }
 
-    return paths.filter(function(path){
-
-        return path.length <= shortestPathLength;
-    });
+  return output;
 };
 
 var findLadders = function(start, end, dict){
-    var graph = buildGraph(end, start, dict);
-    var paths = getPaths(start, graph);
+  var graph = buildGraph(end, dict);
+  var paths = getPaths(start, end, graph, dict);
 
-    return paths;
+  return paths;
 };
 
 
@@ -112,17 +93,10 @@ var dict;
 
 start = 'hit';
 end = 'cog';
-dict = ['hot','dot','dog','lot','log'];
+dict = new Set(['hot','dot','dog','lot','log']);
 console.log(findLadders(start, end, dict));
-start = 'hit';
-end = 'cog';
-dict = ['hot','dot','dog','lot','log','dof','mit','sit','set','mog','mig','seg','nax','max'];
-console.log(findLadders(start, end, dict));
+
 start = 'cet';
 end = 'ism';
 dict = ['kid','tag','pup','ail','tun','woo','erg','luz','brr','gay','sip','kay','per','val','mes','ohs','now','boa','cet','pal','bar','die','war','hay','eco','pub','lob','rue','fry','lit','rex','jan','cot','bid','ali','pay','col','gum','ger','row','won','dan','rum','fad','tut','sag','yip','sui','ark','has','zip','fez','own','ump','dis','ads','max','jaw','out','btu','ana','gap','cry','led','abe','box','ore','pig','fie','toy','fat','cal','lie','noh','sew','ono','tam','flu','mgm','ply','awe','pry','tit','tie','yet','too','tax','jim','san','pan','map','ski','ova','wed','non','wac','nut','why','bye','lye','oct','old','fin','feb','chi','sap','owl','log','tod','dot','bow','fob','for','joe','ivy','fan','age','fax','hip','jib','mel','hus','sob','ifs','tab','ara','dab','jag','jar','arm','lot','tom','sax','tex','yum','pei','wen','wry','ire','irk','far','mew','wit','doe','gas','rte','ian','pot','ask','wag','hag','amy','nag','ron','soy','gin','don','tug','fay','vic','boo','nam','ave','buy','sop','but','orb','fen','paw','his','sub','bob','yea','oft','inn','rod','yam','pew','web','hod','hun','gyp','wei','wis','rob','gad','pie','mon','dog','bib','rub','ere','dig','era','cat','fox','bee','mod','day','apr','vie','nev','jam','pam','new','aye','ani','and','ibm','yap','can','pyx','tar','kin','fog','hum','pip','cup','dye','lyx','jog','nun','par','wan','fey','bus','oak','bad','ats','set','qom','vat','eat','pus','rev','axe','ion','six','ila','lao','mom','mas','pro','few','opt','poe','art','ash','oar','cap','lop','may','shy','rid','bat','sum','rim','fee','bmw','sky','maj','hue','thy','ava','rap','den','fla','auk','cox','ibo','hey','saw','vim','sec','ltd','you','its','tat','dew','eva','tog','ram','let','see','zit','maw','nix','ate','gig','rep','owe','ind','hog','eve','sam','zoo','any','dow','cod','bed','vet','ham','sis','hex','via','fir','nod','mao','aug','mum','hoe','bah','hal','keg','hew','zed','tow','gog','ass','dem','who','bet','gos','son','ear','spy','kit','boy','due','sen','oaf','mix','hep','fur','ada','bin','nil','mia','ewe','hit','fix','sad','rib','eye','hop','haw','wax','mid','tad','ken','wad','rye','pap','bog','gut','ito','woe','our','ado','sin','mad','ray','hon','roy','dip','hen','iva','lug','asp','hui','yak','bay','poi','yep','bun','try','lad','elm','nat','wyo','gym','dug','toe','dee','wig','sly','rip','geo','cog','pas','zen','odd','nan','lay','pod','fit','hem','joy','bum','rio','yon','dec','leg','put','sue','dim','pet','yaw','nub','bit','bur','sid','sun','oil','red','doc','moe','caw','eel','dix','cub','end','gem','off','yew','hug','pop','tub','sgt','lid','pun','ton','sol','din','yup','jab','pea','bug','gag','mil','jig','hub','low','did','tin','get','gte','sox','lei','mig','fig','lon','use','ban','flo','nov','jut','bag','mir','sty','lap','two','ins','con','ant','net','tux','ode','stu','mug','cad','nap','gun','fop','tot','sow','sal','sic','ted','wot','del','imp','cob','way','ann','tan','mci','job','wet','ism','err','him','all','pad','hah','hie','aim','ike','jed','ego','mac','baa','min','com','ill','was','cab','ago','ina','big','ilk','gal','tap','duh','ola','ran','lab','top','gob','hot','ora','tia','kip','han','met','hut','she','sac','fed','goo','tee','ell','not','act','gil','rut','ala','ape','rig','cid','god','duo','lin','aid','gel','awl','lag','elf','liz','ref','aha','fib','oho','tho','her','nor','ace','adz','fun','ned','coo','win','tao','coy','van','man','pit','guy','foe','hid','mai','sup','jay','hob','mow','jot','are','pol','arc','lax','aft','alb','len','air','pug','pox','vow','got','meg','zoe','amp','ale','bud','gee','pin','dun','pat','ten','mob'];
-console.log(findLadders(start, end, dict));
-start = 'a';
-end = 'c';
-dict = ['a', 'b', 'c'];
 console.log(findLadders(start, end, dict));
