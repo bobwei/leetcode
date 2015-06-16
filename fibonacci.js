c21df24df790dac8a4ec6e7859f19248a88e1c89
bobwei <bob61016@gmail.com>
2015-06-15 17:14:30 -0700

    update fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,16 +1,16 @@
 'use strict';
 
 var fibonacci = function(n){
-    var results = {
-        '1': 1,
-        '2': 2
-    };
-
-    for ( var i = 3 ; i <= n ; i++ ){
-        results[i] = results[i-2] + results[i-1];
-    }
-
-    return results[n];
+  var results = {
+    '0': 0,
+    '1': 1
+  };
+  var i = 2;
+  while (i <= n){
+    results[i] = results[i-1] + results[i-2];
+    i++;
+  }
+  return results[n];
 };
 
-console.log(fibonacci(30));
+console.log(fibonacci(100));
