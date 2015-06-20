568a7cf89db9f9bf21f0640e9e7f356a50f1bdd5
bobwei <bob61016@gmail.com>
2015-06-20 03:24:03 -0700

    update island

--- number_of_islands.js
+++ number_of_islands.js
@@ -1,55 +1,87 @@
 'use strict';
 
 var numIslands = function(grid) {
-    var n = grid.length;
-    if (n === 0){
-
-        return 0;
-    }
-    var m = grid[0].length;
-    if (m === 0){
-
-        return 0;
-    }
-
-    var keysCount = 0;
-    var islands = {};
-    grid.forEach(function(row, i){
-        row.forEach(function(column, j){
-            var key = j + ',' + i;
-            if (column){
-                if (islands[key] === undefined){
-                    islands[key] = keysCount;
-                    keysCount++;
-                }
-                if (j + 1 < m){
-                    var nextXKey = ( j + 1 ) + ',' + i;
-                    islands[nextXKey] = islands[key];
-                }
-                if (i + 1 < n){
-                    var nextYKey = j + ',' + ( i + 1 );
-                    islands[nextYKey] = islands[key];
-                }
-            }else{
-                delete islands[key];
+  var n = grid.length;
+  if (!n){
+    return 0;
+  }
+  var m = grid[0].length;
+  if (!m){
+    return 0;
+  }
+  var nIslands = 0;
+  var output = {};
+  for ( var i = 0 ; i < n ; i++ ){
+    for ( var j = 0 ; j < m ; j++ ){
+      // if it's island and not yet visited
+      if (parseInt(grid[i][j], 10) > 0 && output[i + ',' + j] === undefined){
+        nIslands += 1;
+        var queue = [[j,i]];
+        while (queue.length){
+          var current = queue.shift();
+          var x = current[0];
+          var y = current[1];
+          output[y + ',' + x] = nIslands;
+          // check left
+          if (x - 1 >= 0){
+            if (parseInt(grid[y][x - 1], 10) > 0 &&
+                output[y + ',' + (x - 1)] === undefined){
+              output[y + ',' + (x - 1)] = -1;
+              queue.push([x - 1, y]);
             }
-        });
-    });
-
-    return keysCount;
+          }
+          // check right
+          if (x + 1 < m){
+            if (parseInt(grid[y][x + 1], 10) > 0 &&
+                output[y + ',' + (x + 1)] === undefined){
+              output[y + ',' + (x + 1)] = -1;
+              queue.push([x + 1, y]);
+            }
+          }
+          // check top
+          if (y - 1 >= 0 ){
+            if (parseInt(grid[y - 1][x], 10) > 0 &&
+                output[(y - 1) + ',' + x] === undefined){
+              output[(y - 1) + ',' + x] = -1;
+              queue.push([x, y - 1]);
+            }
+          }
+          // check bottom
+          if (y + 1 < n){
+            if (parseInt(grid[y + 1][x], 10) > 0 &&
+                output[(y + 1) + ',' + x] === undefined){
+              output[(y + 1) + ',' + x] = -1;
+              queue.push([x, y + 1]);
+            }
+          }
+        }
+      }
+    }
+  }
+  return nIslands;
 };
 
 var data;
-data =[
-    [1,1,0,0,0],
-    [1,1,0,0,0],
-    [0,0,1,0,0],
-    [0,0,0,1,1]
+
+data = [
+  '11000',
+  '11000',
+  '00100',
+  '00011'
 ];
 console.log(numIslands(data));
 
-data = [[0]];
+data = [
+  '11110',
+  '11010',
+  '11000',
+  '00000'
+];
 console.log(numIslands(data));
 
-data = ["0"];
+data = [
+  '111',
+  '010',
+  '111'
+];
 console.log(numIslands(data));
