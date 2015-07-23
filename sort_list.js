d6d704810bbaa675fa508daf4a91885111354cd8
bobwei <bob61016@gmail.com>
2015-07-23 21:57:58 +0800

    improve sort list

--- sort_list.js
+++ sort_list.js
@@ -1,19 +1,13 @@
 'use strict';
 
+var sll = require('./sll');
+var ListNode = sll.ListNode;
+
 var mergeList = function(head1, head2){
-  if (!head1 || !head2){
-    return head1 || head2;
-  }
-  var head, tail;
-  if (head1.val < head2.val){
-    head = head1;
-    head1 = head1.next;
-  }else{
-    head = head2;
-    head2 = head2.next;
-  }
-  tail = head;
-  tail.next = null;
+  // create an empty node
+  var head = new ListNode();
+  var tail = head;
+
   while (head1 && head2){
     if (head1.val < head2.val){
       tail.next = head1;
@@ -24,30 +18,19 @@ var mergeList = function(head1, head2){
     }
     tail = tail.next;
   }
-  if (head1){
-    tail.next = head1;
-  }else{
-    tail.next = head2;
-  }
-  return head;
+  tail.next = head1 || head2;
+
+  return head.next;
 };
 
 var getMiddleNode = function(head){
-  var current = head;
-  var length = 0;
-  while (current){
-    length += 1;
-    current = current.next;
+  var slowPointer = head;
+  var fastPointer = head;
+  while (slowPointer.next && (fastPointer.next && fastPointer.next.next)){
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
   }
-
-  current = head;
-  var n = Math.floor(length / 2);
-  var j = 0;
-  while (j < n && n > 2){
-    j += 1;
-    current = current.next;
-  }
-  return current;
+  return slowPointer;
 };
 
 var sortList = function(head){
@@ -65,14 +48,38 @@ var sortList = function(head){
   );
 };
 
-var sll = require('./sll');
-var ListNode = sll.ListNode;
-
 var data;
 var headNode;
 data = [3, 5, 4, 2, 1, 9, 2];
 headNode = new ListNode(data);
 console.log(headNode.traverse());
+
+// test merge list
 console.log(
-  sortList(headNode).traverse()
+  mergeList(
+    new ListNode([1, 3, 5]),
+    new ListNode([2, 4, 6])
+  ).traverse()
+);
+
+// test getMiddleNode
+console.log(
+  'middleNode =',
+  getMiddleNode(
+    new ListNode([1, 2, 3])
+  ).val
+);
+
+console.log(
+  'middleNode =',
+  getMiddleNode(
+    new ListNode([2, 3])
+  ).val
+);
+
+// test sortList
+console.log(
+  sortList(
+    new ListNode(data)
+  ).traverse()
 );
