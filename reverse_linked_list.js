809005cbb48491c634f70574a003acc1a7574045
bobwei <bob61016@gmail.com>
2015-09-14 17:08:49 +0800

    update reverse_linked_list

--- reverse_linked_list.js
+++ reverse_linked_list.js
@@ -1,54 +1,30 @@
 'use strict';
 
-var LinkedNode = function(val){
-  this.val = val;
-  this.next = null;
-};
+var sll = require('./sll');
+var ListNode = sll.ListNode;
 
-LinkedNode.prototype.insert = function(val){
-  var current = this;
+var reverseList = function(head) {
+  if (!head){
+    return null;
+  }
+  if (!head.next){
+    return head;
+  }
+  var reversedList = reverseList(head.next);
+
+  var current = reversedList;
   while (current.next){
     current = current.next;
   }
-  current.next = new LinkedNode(val);
+  current.next = head;
+  head.next = null;
+
+  return reversedList;
 };
 
-LinkedNode.prototype.print = function(){
-  var output = [];
-  var current = this;
-  while (current){
-    output.push(current.val);
-    current = current.next;
-  }
-  console.log(output.join('->'));
-};
+var data;
+var theNode;
 
-var reverseLinkedList = function(node){
-  // if it's the last one, return it
-  if (node.next === null){
-    return node;
-  }
-
-  // reverse the rest
-  var reversedNode = reverseLinkedList(node.next);
-
-  // link the last element of the reversed node to first node
-  var current = reversedNode;
-  while (current.next){
-    current = current.next;
-  }
-  current.next = node;
-
-  // delete the link of first node
-  node.next = null;
-  return reversedNode;
-};
-
-var node = new LinkedNode(0);
-for ( var i = 1 ; i < 10 ; i++ ){
-  node.insert(i);
-}
-node.print();
-
-var reversedNode = reverseLinkedList(node);
-reversedNode.print();
+data = [1, 2, 3, 4, 5];
+theNode = new ListNode(data);
+console.log(reverseList(theNode).traverse());
