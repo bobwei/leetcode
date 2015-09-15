f445d39a395d429a38eabeeb0ba94069e37c32bd
bobwei <bob61016@gmail.com>
2015-09-15 16:03:37 +0800

    update construct_binary_tree_from_preorder_and_inorder_traversal with space complexity O(1)

--- construct_binary_tree_from_preorder_and_inorder_traversal.js
+++ construct_binary_tree_from_preorder_and_inorder_traversal.js
@@ -3,38 +3,44 @@
 var bt = require('./bt');
 var TreeNode = bt.TreeNode;
 
-var buildTree = function(preorder, inorder){
-  // console.log(preorder, inorder);
-  if (preorder.length === 1 && inorder.length === 1){
-    return new TreeNode(inorder[0]);
-  }else if (preorder.length === 0 && inorder.length === 0){
+var buildFunc = function(preorder, inorder, m, n, p, q) {
+  // console.log(m, n, p, q);
+  if (q - p === 1){
+    return new TreeNode(inorder[p]);
+  }else if (q - p === 0){
     return null;
   }
-  var val = preorder.shift();
-  var valIndexAtInorder = inorder.indexOf(val);
-  inorder = [
-    inorder.slice(0, valIndexAtInorder),
-    inorder.slice(valIndexAtInorder + 1)
-  ];
-  var left = buildTree(
-    preorder.slice(0, inorder[0].length),
-    inorder[0]
+  var rIndex = inorder.indexOf(preorder[m]);
+  var leftTree = buildFunc(
+    preorder, inorder,
+    m + 1, m + 1 + (rIndex - p),
+    p, rIndex
   );
-  var right = buildTree(
-    preorder.slice(inorder[0].length),
-    inorder[1]
+  var rightTree = buildFunc(
+    preorder, inorder,
+    m + (rIndex - p) + 1, n,
+    rIndex + 1, q
   );
-  var rootNode = new TreeNode(val);
-  rootNode.left = left;
-  rootNode.right = right;
+  var rootNode = new TreeNode(preorder[m]);
+  rootNode.left = leftTree;
+  rootNode.right = rightTree;
   return rootNode;
 };
+
+var buildTree = function(preorder, inorder){
+  return buildFunc(
+    preorder, inorder,
+    0, preorder.length,
+    0, inorder.length
+  );
+};
+
 
 var data;
 var treeNode;
 var resultNode;
 
-data = [0, 1, 2, 3, 4, 5, 6];
+data = [1, 2, 3, 4, 5, 6, 7];
 treeNode = new TreeNode(data);
 resultNode = buildTree(treeNode.preOrder(), treeNode.inOrder());
 console.log(resultNode.inOrder());
