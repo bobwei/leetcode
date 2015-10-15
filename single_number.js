030db801cfce31f8e1bf0cb1e06201740596a65b
bobwei <bob61016@gmail.com>
2015-10-15 16:24:04 +0800

    update single number with reduce function

--- single_number.js
+++ single_number.js
@@ -1,11 +1,9 @@
 'use strict';
 
 var singleNumber = function(nums) {
-  var r = 0;
-  nums.forEach(function(num){
-    r = r ^ num;
-  });
-  return r;
+  return nums.reduce(function(s, v){
+    return s ^ v;
+  }, 0);
 };
 
 console.log(singleNumber([1, 1, 2, 2, 3, 4, 4, 5, 5]));
